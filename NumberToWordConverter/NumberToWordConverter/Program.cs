d237e224f382867bec5caa446800d46fdc12e1aa
Anand Vashishtha <anand.vashishtha@gmail.com>
2019-08-27 22:00:29 +0530

    Added Number to word converter

---
+++ NumberToWordConverter/NumberToWordConverter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SampleUtilityApplication
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            try
+            {
+                ConsoleKeyInfo cki;
+
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please type integer between 0 to 1000000000");
+                    input = Console.ReadLine();
+                    int typedInt = Int32.Parse(input);
+
+                    if (typedInt >= 0 && typedInt < 1000000000)
+                    {
+                        Console.WriteLine(Utility.NumberToWordsConverter(typedInt));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Typed integer is beyond the range");
+                    }
+
+                    Console.WriteLine("Press the Escape (Esc) key to quit or press any key to continue: \n");
+                    cki = Console.ReadKey();
+
+
+                } while (cki.Key != ConsoleKey.Escape);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse '{input}'");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unknown exception");
+            }
+        }
+
+        
+    }
+}
---
+++ NumberToWordConverter/NumberToWordConverter/Utility.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace SampleUtilityApplication
+{
+    public class Utility
+    {
+        public static string NumberToWordsConverter(int number)
+        {
+            if (number == 0)
+            {
+                return "zero";
+            }
+
+            if (number < 0)
+            {
+                return "minus " + NumberToWordsConverter(Math.Abs(number));
+            }
+
+            string words = string.Empty;
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWordsConverter(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWordsConverter(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWordsConverter(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != string.Empty)
+                {
+                    words += "and ";
+                }
+
+                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                {
+                    words += unitsMap[number];
+                }
+                else
+                {
+                    words += tensMap[number / 10];
+
+                    if ((number % 10) > 0)
+                    {
+                        words += " " + unitsMap[number % 10];
+                    }
+                }
+            }
+
+            return words;
+        }
+    }
+}
